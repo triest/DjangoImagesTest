83aeab3d2ede04b2589ef751bd61704e88977ca8
triest <you@example.com>
2020-06-27 20:23:54 +0300

    Add pagination

--- Images/views.py
+++ Images/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.http import Http404
 from django.shortcuts import render, redirect
 from . import forms
@@ -9,6 +10,19 @@ def ImagesListView(request):
     template_name = 'articles/article_list.html'
     # queryset = Article.objects.all().order_by('-date')
     images = Image.objects.all().order_by('-date')
+
+
+    paginator = Paginator(images, 2)  # Show 25 contacts per page
+
+    page = request.GET.get('page')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        images = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        images = paginator.page(paginator.num_pages)
 
     return render(request, 'images/images_list.html', {'images': images})
     # return HttpResponse('homepage')
