14f4432786a2b7ac57091028f8f095dabbd66a0b
triest <you@example.com>
2020-06-28 23:15:22 +0300

    Add form validate

--- DоnjoTest/urls.py
+++ DоnjoTest/urls.py
@@ -17,16 +17,18 @@ from django.contrib import admin
 from django.urls import path
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url,include
-#from . import templates;
+from django.conf.urls import url, include
+# from . import templates;
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/images/')),
     path('admin/', admin.site.urls),
     path('images/', include('images.urls')),
 ]
 
-urlpatterns+=staticfiles_urlpatterns();
-urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns += staticfiles_urlpatterns();
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Images/forms.py
+++ Images/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.core.files.images import get_image_dimensions
+
 from . import models
 
 
@@ -9,9 +11,30 @@ class ImageForm(forms.ModelForm):
         name = forms.CharField(label='Title', max_length=100)
         image = forms.CharField(label='Image', max_length=100)
 
+    def clean_title(self):
+        title = self.cleaned_data.get("title")
+        if not title:
+            return forms.ValidationError("title is requared")
+        else:
+            return title
+
+    def clean_image(self):
+        image = self.cleaned_data.get("image")
+        if not image:
+            raise forms.ValidationError("No image!")
+        else:
+            return image
+
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = models.Comment
         fields = ['title']
         title = forms.CharField(label='Comment text', max_length=100)
+
+    def clean_title(self):
+        title = self.cleaned_data.get("title")
+        if not title:
+            return forms.ValidationError("title is requared")
+        else:
+            return title
